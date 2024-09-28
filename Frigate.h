c4a0c94b31fca3d80e23c80a9163500bafc2d6aa
SirFrog64 <a1923571@adelaide.edu.au>
2024-09-28 12:40:35 +0930

    Add files via upload

---
+++ Frigate.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <time.h>
+#include <random>
+#include "Ship.h"
+
+#ifndef FRIGATE_H
+#define FRIGATE_H
+
+class Frigate : public Ship {
+    
+    private:
+        sf::Texture frigate;
+        sf::Sprite frigateSprite;
+
+    public:
+        Frigate(int size, int cost, int health, int attack_power, int rank, int hit_prob, sf::RenderWindow* window) : Ship(size, cost, health, attack_power, rank, hit_prob, window) {}
+
+        void render(int position_x, int position_y);
+
+        void set_sprite_position(int position_x, int position_y);
+
+        void draw();
+
+        void attack(Ship* target);
+
+};
+
+#endif
